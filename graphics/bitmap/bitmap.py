7e45dd17046038187122355d140df102758fca9f
Artem Penteskul <artempenteskul@Macs-MacBook-Pro.local>
2022-03-11 20:19:41 +0200

    bitmap

---
+++ graphics/bitmap/bitmap.py
@@ -0,0 +1,21 @@
+import sys
+
+
+print('This is bitmap program')
+print('Enter the message to display with the bitmap')
+message = input('> ')
+
+if message == '':
+    print('You haven\'t entered the message. Start program again')
+    sys.exit()
+
+
+file = open('bitmapworld.txt', 'r')
+for line in file:
+    for elementIndex, elementValue in enumerate(line):
+        if elementValue == ' ':
+            print(' ', end='')
+        else:
+            print(message[elementIndex % len(message)], end='')
+    print()
+file.close()
