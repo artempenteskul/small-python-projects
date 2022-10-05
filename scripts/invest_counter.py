503b6e1a747d4e1057b137d1b6ce15b6d83e6f75
Artem Penteskul <artemiy.penteskul@gmail.com>
2022-10-05 22:05:12 +0300

    scripts: invest_counter 2

--- scripts/invest_counter.py
+++ scripts/invest_counter.py
@@ -5,6 +5,7 @@ ANNUAL_DIVIDEND_PERCENT = 0.025
 def main():
     print('The Invest Counter Script!')
     print('This is simple (silly) invest counter script which counts your minimal invest earnings.')
+    print()
 
     invest_duration = get_invest_duration()
     annual_replenishment = get_annual_replenishment()
@@ -28,23 +29,28 @@ def main():
         capital += (annual_earnings + annual_dividends)
 
         print(f'--- YEAR #{i+1} ---')
-        print(f'Start year capital: ${start_year_capital}.')
-        print(f'Annual earnings: ${annual_earnings}.')
-        print(f'Annual dividends: ${annual_dividends}.')
-        print(f'End year capital: ${capital}.')
+        print(f'Start year capital: ${start_year_capital:.2f}.')
+        print(f'Annual earnings: ${annual_earnings:.2f}.')
+        print(f'Annual dividends: ${annual_dividends:.2f}.')
+        print(f'End year capital: ${capital:.2f}.')
+
+        capital += annual_replenishment
+
+        input('Press Enter to continue ...')
+        print()
 
     print()
 
-    print(f'--- Overall results for {invest_duration} years ---')
-    print(f'Start capital: ${annual_replenishment}.')
-    print(f'End capital: ${capital}.')
-    print(f'Total earnings sum: ${earnings}.')
-    print(f'Dividends sum: ${dividends}.')
+    print(f'--- Overall results for {invest_duration:.2f} years ---')
+    print(f'Start capital: ${annual_replenishment:.2f}.')
+    print(f'End capital: ${capital:.2f}.')
+    print(f'Total earnings sum: ${earnings:.2f}.')
+    print(f'Dividends sum: ${dividends:.2f}.')
 
     print()
 
-    print(f'Money spent: ${invest_duration * annual_replenishment}.')
-    print(f'Money earned: ${capital - (invest_duration * annual_replenishment)}.')
+    print(f'Money spent: ${invest_duration * annual_replenishment:.2f}.')
+    print(f'Money earned: ${capital - (invest_duration * annual_replenishment):.2f}.')
 
 
 def get_invest_duration():
@@ -52,7 +58,6 @@ def get_invest_duration():
     while not response.isdecimal():
         response = input('Enter how many years do you wanna invest: ')
 
-    print()
     print(f'Program takes your invest duration as {int(response)} years.')
 
     return int(response)
@@ -63,7 +68,6 @@ def get_annual_replenishment():
     while not response.isdecimal():
         response = input('Enter how much money per year can you add in average to your capital in $: ')
 
-    print()
     print(f'Program takes your annual replenishment as ${float(response)}')
 
     return float(response)
