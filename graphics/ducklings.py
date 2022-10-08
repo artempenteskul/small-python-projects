268edc614b065780b2602de4b59b8ef6f3fe33e4
Artem Penteskul <artemiy.penteskul@gmail.com>
2022-10-08 17:28:09 +0300

    graphics: ducklings 3

--- graphics/ducklings.py
+++ graphics/ducklings.py
@@ -54,7 +54,114 @@ def main():
 
 
 class Duckling:
-    pass
+    def __init__(self):
+        self.direction = random.choice([LEFT, RIGHT])
+        self.body = random.choice([CHUBBY, VERY_CHUBBY])
+        self.mouth = random.choice([OPEN, CLOSED])
+        self.wing = random.choice([OUT, UP, DOWN])
+
+        if self.body == CHUBBY:
+            self.eyes = BEADY
+        else:
+            self.eyes = random.choice([BEADY, WIDE, HAPPY, ALOOF])
+
+        self.part_to_display_next = HEAD
+
+    def get_head_str(self):
+        head_str = ''
+        if self.direction == LEFT:
+            if self.mouth == OPEN:
+                head_str += '>'
+            elif self.mouth == CLOSED:
+                head_str += '='
+
+            if self.eyes == BEADY and self.body == CHUBBY:
+                head_str += '"'
+            elif self.eyes == BEADY and self.body == VERY_CHUBBY:
+                head_str += '" '
+            elif self.eyes == WIDE:
+                head_str += "''"
+            elif self.eyes == HAPPY:
+                head_str += '^^'
+            elif self.eyes == ALOOF:
+                head_str += '``'
+
+            head_str += ') '
+
+        elif self.direction == RIGHT:
+            head_str += ' ('
+
+            if self.eyes == BEADY and self.body == CHUBBY:
+                head_str += '"'
+            elif self.eyes == BEADY and self.body == VERY_CHUBBY:
+                head_str += ' "'
+            elif self.eyes == WIDE:
+                head_str += "''"
+            elif self.eyes == HAPPY:
+                head_str += '^^'
+            elif self.eyes == ALOOF:
+                head_str += '``'
+
+            if self.mouth == OPEN:
+                head_str += '<'
+            elif self.mouth == CLOSED:
+                head_str += '='
+
+        if self.body == CHUBBY:
+            head_str += ' '
+
+        return head_str
+
+    def get_body_str(self):
+        body_str = '('
+        if self.direction == LEFT:
+            if self.body == CHUBBY:
+                body_str += ' '
+            elif self.body == VERY_CHUBBY:
+                body_str += '  '
+
+            if self.wing == OUT:
+                body_str += '>'
+            elif self.wing == UP:
+                body_str += '^'
+            elif self.wing == DOWN:
+                body_str += 'v'
+
+        if self.direction == RIGHT:
+            if self.wing == OUT:
+                body_str += '<'
+            elif self.wing == UP:
+                body_str += '^'
+            elif self.wing == DOWN:
+                body_str += 'v'
+
+            if self.body == CHUBBY:
+                body_str += ' '
+            elif self.body == VERY_CHUBBY:
+                body_str += '  '
+
+        body_str += ')'
+        if self.body == CHUBBY:
+            body_str += ' '
+
+        return body_str
+
+    def get_feet_str(self):
+        if self.body == CHUBBY:
+            return ' ^^ '
+        elif self.body == VERY_CHUBBY:
+            return ' ^ ^ '
+
+    def get_next_body_part(self):
+        if self.part_to_display_next == HEAD:
+            self.part_to_display_next = BODY
+            return self.get_head_str()
+        elif self.part_to_display_next == BODY:
+            self.part_to_display_next = FEET
+            return self.get_body_str()
+        elif self.part_to_display_next == FEET:
+            self.part_to_display_next = None
+            return self.get_feet_str()
 
 
 if __name__ == '__main__':
