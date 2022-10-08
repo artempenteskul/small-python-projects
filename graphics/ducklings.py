1bf27a9a259dc6acb0c51799e581e36a20579c2f
Artem Penteskul <artemiy.penteskul@gmail.com>
2022-10-08 16:51:22 +0300

    graphics: ducklings 2

--- graphics/ducklings.py
+++ graphics/ducklings.py
@@ -29,9 +29,32 @@ WIDTH = shutil.get_terminal_size()[0] - 1
 
 
 def main():
-    print('The Duckling Graphics Program!')
+    print('The Ducklings Graphics Program!')
     print('Press Ctrl-C to QUIT.')
     time.sleep(2)
+
+    duckling_lanes = [None] * (WIDTH // DUCKLING_WIDTH)
+
+    while True:
+        for lane_num, duckling_obj in enumerate(duckling_lanes):
+            if duckling_obj is None and random.random() <= DENSITY:
+                duckling_obj = Duckling()
+                duckling_lanes[lane_num] = duckling_obj
+
+            if duckling_obj is not None:
+                print(duckling_obj.get_next_body_part(), end='')
+                if duckling_obj.part_to_display_next is None:
+                    duckling_lanes[lane_num] = None
+            else:
+                print(' ' * DUCKLING_WIDTH, end='')
+
+        print()
+        sys.stdout.flush()
+        time.sleep(PAUSE)
+
+
+class Duckling:
+    pass
 
 
 if __name__ == '__main__':
