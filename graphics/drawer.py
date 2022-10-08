efec1196e313ea3373a2ed5f345c7d8f15a46ec8
Artem Penteskul <artemiy.penteskul@gmail.com>
2022-10-08 18:17:40 +0300

    graphics: drawer 2

--- graphics/drawer.py
+++ graphics/drawer.py
@@ -18,9 +18,58 @@ CANVAS_WIDTH, CANVAS_HEIGHT = shutil.get_terminal_size()
 CANVAS_WIDTH -= 1
 CANVAS_HEIGHT -= 5
 
+canvas = {}
+cursor_x = 0
+cursor_y = 0
+
 
 def main():
-    ...
+    moves = []
+    while True:
+        print(get_canvas_string(canvas, cursor_x, cursor_y))
+        print('WASD keys to move, H for help, C to clear, F to save, Q to quit.')
+        response = input('> ').upper()
+
+        if response == 'Q':
+            print('Thanks for playing!')
+            sys.exit()
+
+
+def get_canvas_string(canvas_data, x, y):
+    canvas_str = ''
+    for row_num in range(CANVAS_HEIGHT):
+        for column_num in range(CANVAS_WIDTH):
+            if column_num == x and row_num == y:
+                canvas_str += '#'
+                continue
+
+            cell = canvas_data.get((column_num, row_num))
+            if cell in ({'W', 'S'}, {'W'}, {'S'}):
+                canvas_str += UP_DOWN_CHAR
+            elif cell in ({'A', 'D'}, {'A'}, {'D'}):
+                canvas_str += LEFT_RIGHT_CHAR
+            elif cell == {'S', 'D'}:
+                canvas_str += DOWN_RIGHT_CHAR
+            elif cell == {'A', 'S'}:
+                canvas_str += DOWN_LEFT_CHAR
+            elif cell == {'W', 'D'}:
+                canvas_str += UP_RIGHT_CHAR
+            elif cell == {'W', 'A'}:
+                canvas_str += UP_LEFT_CHAR
+            elif cell == {'W', 'S', 'D'}:
+                canvas_str += UP_DOWN_RIGHT_CHAR
+            elif cell == {'A', 'S', 'D'}:
+                canvas_str += DOWN_LEFT_RIGHT_CHAR
+            elif cell == {'W', 'A', 'D'}:
+                canvas_str += UP_LEFT_RIGHT_CHAR
+            elif cell == {'W', 'A', 'S', 'D'}:
+                canvas_str += CROSS_CHAR
+            elif cell is None:
+                canvas_str += ' '
+
+        canvas_str += '\n'
+
+    return canvas_str
 
 
 if __name__ == '__main__':
