8fb4033c0e13ef82d85ad0c300fb422945b8a6c5
Artem Penteskul <artemiy.penteskul@gmail.com>
2022-10-09 17:07:10 +0300

    games: fast_draw 3

--- games/fast_draw.py
+++ games/fast_draw.py
@@ -12,6 +12,8 @@ def main():
     print()
     input('Press Enter to begin ...')
 
+    score = {'victory': 0, 'loss': 0}
+
     while True:
         print()
         print('It\'s a high noon ...')
@@ -23,17 +25,22 @@ def main():
 
         if time_res < 0.01:
             print('You drew before the input! You lose!')
+            score['loss'] += 1
         elif time_res > 0.3:
             time_res = round(time_res, 4)
             print(f'You took {time_res} seconds to draw, too slow! You lose!')
+            score['loss'] += 1
         else:
             time_res = round(time_res, 4)
             print(f'You took {time_res} seconds to draw!')
             print('You are the fastest draw in the west! You win!')
+            score['victory'] += 1
 
         print('Enter QUIT to stop, or press Enter to play another round: ')
         response = input('> ')
         if response.lower().startswith('q'):
+            print('--- SCORE ---')
+            print(f'{score["victory"]} VICTORIES - {score["loss"]} LOSSES!')
             sys.exit()
 
 
