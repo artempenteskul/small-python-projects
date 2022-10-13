ffbde3a89e19ddd15dd67c1ff42379765abbf4a3
Artem Penteskul <artemiy.penteskul@gmail.com>
2022-10-13 20:48:57 +0300

    games: hangman 1

---
+++ games/hangman.py
@@ -0,0 +1,38 @@
+import sys
+
+
+NUMBER_OF_ATTEMPTS = 10
+
+
+def main():
+    print('The Hangman Game!')
+    print('It\'s a game for two players.')
+    print('The goal of the game is to guess word, which another player thought of.')
+    print('You can guess it by a letters (like in usual hangman game), and you\'l also have the category of word.')
+    print('By default, you have 10 attempts to guess the word.')
+    print()
+
+    number_of_rounds = get_rounds_number()
+    number_of_games = number_of_rounds * 2
+
+
+def get_rounds_number():
+    print('At first, we need to decide how many you want to play: ')
+    response = input('> ')
+
+    while not response.isdecimal() and int(response) > 0:
+        if response.lower().startswith('q'):
+            sys.exit()
+
+        print('You need to enter how many rounds do you wanna play:  (Q to quit)')
+        response = input('> ')
+
+    return int(response)
+
+
+def clear_screen():
+    print('\n' * 100)
+
+
+if __name__ == '__main__':
+    main()
