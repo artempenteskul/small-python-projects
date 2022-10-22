5d3a420e388895bec2080daa89a199a294c01c27
Artem Penteskul <artemiy.penteskul@gmail.com>
2022-10-22 15:23:24 +0300

    scripts: simple_sub_cipher 2

--- scripts/simple_sub_cipher.py
+++ scripts/simple_sub_cipher.py
@@ -63,5 +63,51 @@ def main():
         sys.exit()
 
 
+def check_key(key):
+    key_list = list(key)
+    letters_list = list(LETTERS)
+    key_list.sort()
+    letters_list.sort()
+    if key_list != letters_list:
+        print('There is an error in the key or symbol set. Try another key.')
+        return False
+    return True
+
+
+def encrypt_message(message, key):
+    return translate_message(message, key, mode='encrypt')
+
+
+def decrypt_message(message, key):
+    return translate_message(message, key, mode='decrypt')
+
+
+def translate_message(message, key, mode):
+    translated = ''
+    normal_letters_order = LETTERS
+    key_letters_order = key
+
+    if mode == 'decrypt':
+        normal_letters_order, key_letters_order = key_letters_order, normal_letters_order
+
+    for char in message:
+        if char.upper() in normal_letters_order:
+            char_index = normal_letters_order.find(char.upper())
+            if char.isupper():
+                translated += key_letters_order[char_index].upper()
+            else:
+                translated += key_letters_order[char_index].lower()
+        else:
+            translated += char
+
+    return translated
+
+
+def generate_random_key():
+    key = list(LETTERS)
+    random.shuffle(key)
+    return ''.join(key)
+
+
 if __name__ == '__main__':
     main()
