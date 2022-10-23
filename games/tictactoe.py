2f0f05258e1c84fddeecb05d71b1d3096c9c034e
Artem Penteskul <artemiy.penteskul@gmail.com>
2022-10-23 12:55:54 +0300

    games: tictactoe 4

--- games/tictactoe.py
+++ games/tictactoe.py
@@ -6,30 +6,31 @@ def main():
     print('Welcome to Tic-Tac-Toe Game')
 
     game_board = get_blank_board()
-    player_1, player_2 = X, O
+    move_player, wait_player = X, O
 
     while True:
         print(get_board_str(game_board))
 
         move = None
         while not is_valid_space(game_board, move):
-            print(f'What\'s is {player_1}\'s move? (1-9)')
+            print(f'What\'s is {move_player}\'s move? (1-9)')
             move = input('> ')
 
-        update_board(game_board, move, player_1)
+        update_board(game_board, move, move_player)
 
-        if is_winner(game_board, player_1):
+        if is_winner(game_board, move_player):
             print(get_board_str(game_board))
-            print(f'{player_2} has won the game!')
+            print(f'{move_player} has won the game!')
             break
         elif is_board_full(game_board):
             print(get_board_str(game_board))
             print('The game is a tie!')
             break
 
-        player_1, player_2 = player_2, player_1
-        print()
-        print('Thanks for playing!')
+        move_player, wait_player = wait_player, move_player
+
+    print()
+    print('Thanks for playing!')
 
 
 def get_blank_board():
@@ -45,12 +46,14 @@ def get_board_str(board):
     --+---+--
     {board['4']} | {board['5']} | {board['6']}   4  5  6
     --+---+-- 
-    {board['9']} | {board['8']} | {board['9']}   7  8  9
+    {board['7']} | {board['8']} | {board['9']}   7  8  9
     '''
 
 
 def is_valid_space(board, space):
-    return True if board[space] == BLANK and space in ALL_SPACES else False
+    if board.get(space) == BLANK and space in ALL_SPACES:
+        return True
+    return False
 
 
 def is_winner(board, player):
